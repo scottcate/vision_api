1c79ff916e2961cef3eac40e891fcde21b040a6f
Scott Cate <Scottca@microsoft.com>
2018-04-04 10:11:25 -0500

    closes #4, closes #6. Better twitter status reply

--- src/VisionScanned.cs
+++ src/VisionScanned.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -20,13 +22,42 @@ namespace TwitterVision.Twitter
             foreach (var vision in dto.MediaDescription.VisionDescription)
             {
                 var jsonUrl = string.Format(Helper.GetEnvironmentVariable("JsonDetailsUrlFormat"), dto.OriginalTweetId);
-                var confidence = $"{vision.Value.description.captions[0].confidence,0:P2}";
+                var confidence = vision.Value.description.captions[0].confidence;
+
+                string captionPrefix= "Caption";
+                if (confidence < .2)
+                    captionPrefix = "Guessing";
+                else if (confidence < .4)
+                    captionPrefix = "Unsure";
+                else if (confidence < .6)
+                    captionPrefix = "Maybe";
+                else if (confidence < .8)
+                    captionPrefix = "Probably";
+
                 var replyToNames = string.Join(" ", dto.UsersToReplyTo);
-                var status = $"{replyToNames} Confidence: {confidence}" +
+                var status = $"{replyToNames} Confidence: {confidence,0:P2}" +
                          $"{Environment.NewLine}{Environment.NewLine}" +
-                         $"Vision API: { vision.Value.description.captions[0].text}" +
+                         "Tags: {0}" +
                          $"{Environment.NewLine}{Environment.NewLine}" +
-                         $"Full API Result: { jsonUrl }";
+                         $"{captionPrefix}: {vision.Value.description.captions[0].text}" +
+                         $"{Environment.NewLine}{Environment.NewLine}" +
+                         "Full API Result: {1}";
+
+                //20 is for the t.co link space
+                //replyToNames are not counted in tweet length
+                var space = 280 - status.Length - 20 + replyToNames.Length;
+                var tags = new List<string>();
+                foreach (var tag in vision.Value.description.tags)
+                {
+                    int length = tag.Length;
+                    if (length + space > 280)
+                        break;
+
+                    space += length + 2; //2 is for the ", " when joined
+                    tags.Add(tag);
+                }
+
+                status = string.Format(status, string.Join(", ", tags), jsonUrl);
 
                 log.Info($"VisionScanned function Sending Tweet: {dto.TwitterStatus.Id} - {status}");
 
